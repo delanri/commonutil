3ed5476350da5bd49c14beeae9ceceaa3c74eefa
gamma <gamma@ovo.id>
2020-08-04 14:45:39 +0700

    remove auto commit conf

--- messaging/kafka-sarama/base.go
+++ messaging/kafka-sarama/base.go
@@ -120,8 +120,6 @@ func (l *Kafka) NewListener() (sarama.ConsumerGroup, error) {
 	config.Version = kfkVersion
 	config.Consumer.Return.Errors = true
 	config.Consumer.MaxWaitTime = l.Option.MaxWait
-	config.Consumer.Offsets.AutoCommit.Enable = l.Option.ConsumerOffsetsAutoCommitEnabled
-	config.Consumer.Offsets.AutoCommit.Interval = l.Option.ConsumerOffsetsAutoCommitInterval
 
 	switch l.Option.Strategy {
 	case "sticky":
